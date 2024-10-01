12061e72fb44bb7a2b43d205adc35af41af090ae
Adam Holt <me@adamholt.co.uk>
2024-10-01 19:24:34 +0200

    Add support for leader mode when leader key is pressed

--- plugin/tabline/components/window/mode.lua
+++ plugin/tabline/components/window/mode.lua
@@ -1,7 +1,12 @@
 return {
   update = function(window)
     local key_table = window:active_key_table() or 'normal_mode'
-    local mode = key_table:gsub('_mode', ''):upper()
+    local mode = ''
+    if window:leader_is_active() then
+      mode = "LEADER"
+    else
+      mode = key_table:gsub('_mode', ''):upper()
+    end
     return mode
   end,
 }
