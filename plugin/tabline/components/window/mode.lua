20196c56b817573be6b1b1b86e6ae6816ea20999
Adam Holt <me@adamholt.co.uk>
2024-10-02 11:35:57 +0200

    Switch this back and use the new leader component

--- plugin/tabline/components/window/mode.lua
+++ plugin/tabline/components/window/mode.lua
@@ -1,12 +1,7 @@
 return {
   update = function(window)
     local key_table = window:active_key_table() or 'normal_mode'
-    local mode = ''
-    if window:leader_is_active() then
-      mode = "LEADER"
-    else
-      mode = key_table:gsub('_mode', ''):upper()
-    end
+    local mode = key_table:gsub('_mode', ''):upper()
     return mode
   end,
 }
