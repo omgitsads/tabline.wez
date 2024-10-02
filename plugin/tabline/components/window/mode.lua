fff428f29011bd79809c6c45500221079e076818
Adam Holt <me@adamholt.co.uk>
2024-10-02 12:38:59 +0200

    Revert mode changes

--- plugin/tabline/components/window/mode.lua
+++ plugin/tabline/components/window/mode.lua
@@ -1,8 +1,7 @@
 return {
   update = function(window)
     local key_table = window:active_key_table()
-    return key_table
-    --local mode = key_table:gsub('_mode', ''):upper()
-    --return mode
+    local mode = key_table:gsub('_mode', ''):upper()
+    return mode
   end,
 }
