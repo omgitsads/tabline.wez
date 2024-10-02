2352b1e3686f06ecec6306f581794d29aea8420a
Adam Holt <me@adamholt.co.uk>
2024-10-02 12:24:01 +0200

    Remove default

--- plugin/tabline/components/window/mode.lua
+++ plugin/tabline/components/window/mode.lua
@@ -1,6 +1,6 @@
 return {
   update = function(window)
-    local key_table = window:active_key_table() or 'normal_mode'
+    local key_table = window:active_key_table()
     return key_table
     --local mode = key_table:gsub('_mode', ''):upper()
     --return mode
